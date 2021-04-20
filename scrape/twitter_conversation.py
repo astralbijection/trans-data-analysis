ab1fdb2fe95a2ddf5e54b30b422113d9dc28f56c
Astrid Yu <astrid@astrid.tech>
2021-04-20 14:33:37 -0700

    Clean and join politician tweets

--- scrape/twitter_conversation.py
+++ scrape/twitter_conversation.py
@@ -1,10 +1,15 @@
 import json
 import os
 from datetime import datetime
+from pprint import pprint
 from typing import List
-
+import requests
 import jsonlines
+import twarc
+import tweepy
+import twitter
 from dotenv import load_dotenv
+from requests_oauthlib import OAuth1Session
 from twarc import Twarc
 
 
@@ -18,38 +23,37 @@ def get_politicians(filepath):
 
 
 def read_timelines(after_date: datetime, handles: List[str], with_replies=False):
-    consumer_key = os.environ.get('consumer_key')
-    consumer_secret = os.environ.get('consumer_secret')
-    access_token_key = os.environ.get('access_token_key')
-    access_token_secret = os.environ.get('access_token_secret')
-    twarc = Twarc(consumer_key, consumer_secret, access_token_key, access_token_secret)
+    consumer_key = os.environ.get('CONSUMER_KEY')
+    consumer_secret = os.environ.get('CONSUMER_SECRET')
+    access_token_key = os.environ.get('ACCESS_TOKEN')
+    access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+    bearer_token = os.environ.get('BEARER_TOKEN')
 
-    for handle in handles:
-        print(f'Scanning twitter handle @{handle}')
-        for tweet in twarc.timeline(screen_name=handle):
-            created_at = parse_twitter_datetime(tweet['created_at'])
-            print(f'Found tweet created at @{created_at}')
+    tw = twarc.Twarc(consumer_key, consumer_secret, access_token_key, access_token_secret, bearer_token)
 
-            if created_at <= after_date:
-                break
+    for tweet in tw.search('Dawkins'):
+        yield tweet
 
-            if not with_replies:
-                yield tweet
-                continue
+        created_at = parse_twitter_datetime(tweet['created_at'])
+        print(f'Found tweet {tweet["id"]} created at {created_at}')
 
-            # Fetch tweet replies
-            for reply in twarc.replies(tweet, recursive=True):
-                print(f'Got reply {reply["id"]}')
-                yield reply
+        if created_at <= after_date:
+            break
+
+
+def store_tweets(tweets):
+    with jsonlines.open('../data/tweetdata.jsonl', 'w') as writer:
+        for tweet in tweets:
+            writer.write(tweet)
+
 
 def main():
     politicians = get_politicians('../data/politicians.json')
     handles = [handle['handle'] for p in politicians for handle in p['twitters']]
 
-    with jsonlines.open('../data/tweetdata.jsonl', 'w') as writer:
-        earliest_date = datetime(2021, 3, 15)
-        for tweet in read_timelines(earliest_date, handles, with_replies=True):
-            writer.write(tweet)
+    earliest_date = datetime(2021, 4, 9)
+    tweets = read_timelines(earliest_date, handles, with_replies=True)
+    store_tweets(tweets)
 
 
 if __name__ == '__main__':
